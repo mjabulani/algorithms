7bd0f3cfc987a055fa7af78c267c9280c81aeda9
mjabulani <marekzmienka@gmail.com>
2023-01-13 14:55:33 +0100

    added exceptions

--- algorithmsLearning/src/Main.java
+++ algorithmsLearning/src/Main.java
@@ -2,20 +2,21 @@ import javax.sound.midi.Soundbank;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        runProgram();
     }
 
-    public static void runProgram() {
+    public static void runProgram() throws Exception {
         System.out.println("Chose the program you want to run\n" +
                 "Type 0 to view list of available programs");
         Scanner s = new Scanner(System.in);
         int program = s.nextInt();
+        runProgram(program);
 
     }
 
     public static void runProgram(int program) throws Exception {
-        //                ListOfAlgorithms.list();
         switch (program) {
             case 0 -> ListOfAlgorithms.list();
             case 1 -> One.printName();

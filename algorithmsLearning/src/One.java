975cc3bdbf564e710a639da69e9ccf58e5e5f54e
mjabulani <marekzmienka@gmail.com>
2023-01-13 14:41:09 +0100

    Commit #3, building structure and learning git.

--- algorithmsLearning/src/One.java
+++ algorithmsLearning/src/One.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class One {
 
     //        #1: Write a Java program to print 'Hello' on screen and then print your name on a separate line.
@@ -5,6 +7,14 @@ public class One {
 //          Hello
 //          Alexandra Abramov
     public static void printName(String name) {
-        System.out.println("Hello\n" + name);
+        System.out.println("Hello " + name);
+    }
+
+    public static void printName() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Provide your name:");
+        String name = scan.nextLine();
+        printName(name);
     }
 }
+

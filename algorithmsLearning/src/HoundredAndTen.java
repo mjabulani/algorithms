fd9e8791ff5bc9db0dec70e4b62e420c964a5899
mjabulani <marekzmienka@gmail.com>
2023-01-30 13:49:08 +0100

    alg138

---
+++ algorithmsLearning/src/HoundredAndTen.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class HoundredAndTen {
+    public static void main(String[] args) {
+       int w = (int) Math.pow(32, 1.0/5.0);
+        System.out.println(w);
+    }
+
+//    public static void isPow4() {
+//        System.out.println("## Test if a given number is a power of 4");
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("Provide the number");
+//        int number = s.nextInt();
+//        System.out.println(isPow4(number));
+//    }
+
+//    public static boolean isPow4(int number) {
+//        double tempValue = 0;
+//        while (tempValue >= number) {
+//            for (int i = 0; tempValue < number; i++) {
+//                tempValue = Math.pow(i, 1.);
+//                if (tempValue == number) {
+//                    return true;
+//                }
+//            }
+//        }
+//    }
+}
---
+++ algorithmsLearning/src/HoundredAndThirtyEight.java
@@ -0,0 +1,42 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class HoundredAndThirtyEight {
+
+    public static void main(String[] args) {
+        longestString();
+    }
+    public static void longestString() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("How many strings to be added?");
+        int stringsCount = s.nextInt();
+        System.out.println(longestString(generateDictionary(stringsCount)));
+
+
+    }
+
+    public static ArrayList<String> generateDictionary(int count) {
+        Scanner a = new Scanner(System.in);
+        ArrayList<String> strings = new ArrayList<String>();
+        for (int i = 0; i < count; i++) {
+            System.out.println("Provide " + i+1 + " string.");
+            String word = a.nextLine();
+            strings.add(word);
+        }
+        return strings;
+    }
+
+    public static String longestString(ArrayList<String> strings) {
+        String max = strings.get(0);
+        for (int i = 1; i < strings.size(); i++) {
+            if (strings.get(i).length() > max.length()) {
+                max = strings.get(i);
+            }
+        }
+        return max;
+    }
+}
+
